d282b8def337f924207dc64a6f93faa5c15fa0bc
William Fondoulis <sm.wfondu@gmail.com>
2025-01-03 15:34:29 -0500

    hide the menu from scene

--- Assets/Scripts/Day1CanvasManager.cs
+++ Assets/Scripts/Day1CanvasManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Day1CanvasManager : MonoBehaviour
 {
     [SerializeField] private GameObject telescopeImage;
-    
+    [SerializeField] private GameObject eyelids;
+
     // Start is called before the first frame update
     void Start()
     {
         telescopeImage.SetActive(true);
+        eyelids.SetActive(true);
     }
 }
